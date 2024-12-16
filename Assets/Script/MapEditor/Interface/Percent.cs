db6e4688bb81a97391caffc58c89e040e8fa9eb0
AKA5144 <150762463+AKA5144@users.noreply.github.com>
2024-12-16 17:27:46 +0100

    add percent

--- Assets/Script/MapEditor/Interface/Percent.cs
+++ Assets/Script/MapEditor/Interface/Percent.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class Percent : MonoBehaviour
 {
     public TextMeshProUGUI text;
+    public SongInfoEditor info;
+    float currentpercent;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,6 +15,8 @@ public class Percent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        currentpercent = (info.timeCode * 100) / info.clipLength;
+        string formattedPercent = currentpercent.ToString("F1");
+        text.text = formattedPercent + " %";
     }
 }
